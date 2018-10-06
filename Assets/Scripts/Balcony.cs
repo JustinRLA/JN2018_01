10285328b742f21ca291b573d683e66a5353810e
Alexis <alexis.morin.mail@gmail.com>
2018-10-06 15:40:37 -0400

    Modular Kit work and balcony edits

---
+++ Assets/Scripts/Balcony.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Balcony : MonoBehaviour {
+
+	public bool canEndGame = false;
+	public Light balconyLight;
+
+	public void OpenBalcony () {
+		GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (0, Random.Range (0f, 100f));
+		GetComponent<BoxCollider> ().isTrigger = true;
+		balconyLight.intensity = 1f;
+		canEndGame = true;
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "intimatePlayer" && canEndGame == true) {
+			SceneManager.LoadScene ("BalconyScene", LoadSceneMode.Single);
+		}
+	}
+
+}
