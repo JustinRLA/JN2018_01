3b36eac71ed9e519c4fcf7e252b223c4d7e04d8d
Alexis <alexis.morin.mail@gmail.com>
2018-10-06 16:54:04 -0400

    New character randomization

---
+++ Assets/Scripts/CharacterRandomizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRandomizer : MonoBehaviour {
+
+	public bool isPlayer;
+	public GameObject[] hats;
+	public GameObject[] collars;
+	public SkinnedMeshRenderer BodyMaterials;
+
+	public Material[] pantsMaterial;
+	public Material[] topsMaterial;
+	public Material[] skinMaterial;
+
+	void Start () {
+
+		if (!isPlayer) {
+			int aDice = Random.Range (0, hats.Length + 1);
+			int bDice = Random.Range (0, collars.Length + 1);
+
+			if (aDice <= hats.Length) {
+				for (int i = 0; i < hats.Length; i++) {
+					if (i != aDice) {
+						hats[i].SetActive (false);
+					}
+				}
+			}
+			if (bDice <= collars.Length) {
+				for (int i = 0; i < collars.Length; i++) {
+					if (i != aDice) {
+						collars[i].SetActive (false);
+					}
+				}
+			}
+		}
+
+	}
+}
